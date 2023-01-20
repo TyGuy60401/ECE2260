d30badb9da5fc5af16e7ee76f504e920bf7cf809
Ty Davis <tydavisxc2019@gmail.com>
2023-01-19 19:48:15 -0700

    Got it all working!

--- Lab1/DAVIS_ece2260_lab1.py
+++ Lab1/DAVIS_ece2260_lab1.py
@@ -4,12 +4,14 @@ def calculate_roots(coef):
     a = coef[0]
     b = coef[1]
     c = coef[2]
-    try:
-        x1 = (-b - math.sqrt(b*b - 4*a*c))/(2*a)
-        x2 = (-b + math.sqrt(b*b - 4*a*c))/(2*a)
-        return (x1, x2)
-    except ValueError as err:
-        return err
+    in_rad = b*b - 4*a*c
+    if (in_rad >= 0):
+        x1 = (-b + math.sqrt(in_rad))/(2*a)
+        x2 = (-b - math.sqrt(in_rad))/(2*a)
+    else:
+        x1 = (-b + 1j*math.sqrt(-in_rad))/(2*a)
+        x2 = (-b - 1j*math.sqrt(-in_rad))/(2*a)
+    return (x1, x2)
     
     
 
@@ -32,25 +34,54 @@ def sum_factorial(n):
         i += 1
     return val
     # to do
-    
+
+def our_function(x):
+    return math.e ** (-3*x) * math.cos(math.pi * x)
         
 def left_riemann(delta_x, lb, ub):
-    return
+    i = lb
+    val = 0
+
+    while i < ub:
+        val += our_function(i) * delta_x
+        i += delta_x
+    return val
     # to do
     
 
 def right_riemann(delta_x, lb, ub):
-    return
+    i = lb + delta_x
+    val = 0
+
+    while i <= ub:
+        val += our_function(i) * delta_x
+        i += delta_x
+
+    return val
     # to do
     
 
 def midpoint_riemann(delta_x, lb, ub):
-    return
+    i = lb + delta_x/2
+    val = 0
+
+    while i < ub:
+        val += our_function(i) * delta_x
+        i += delta_x
+    return val
     # to do
 
     
 def trap_riemann(delta_x, lb, ub):
-    return
+    i = lb
+    val = 0
+    while i < ub:
+        left = i
+        right = i + delta_x
+        val += (our_function(left) + our_function(right))/2*delta_x
+        i += delta_x
+
+    return val
     # to do
 
     
@@ -106,11 +137,9 @@ def main():
     
     for n in [3, 5, 6]:
         output_summation = sum_factorial(n)
-        print(output_summation)
         print("computed factorial summation for n=%i is: %i" %
               (n, output_summation))
     
-    return
         
     ##############################################################
     # Part 4
@@ -140,6 +169,7 @@ def main():
     for delta_x in [1, 0.1, 0.01, 0.0001]:
         summation = trap_riemann(delta_x, lb, ub)
         print("\tdelta_x=%f, summation=%f" % (delta_x, summation))
+    return
 
         
 if __name__ == "__main__":
