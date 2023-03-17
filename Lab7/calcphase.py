6d0e17ccd89368fa7ba1ad8436e8bca5cceb48ad
Ty Davis <tydavisxc2019@gmail.com>
2023-03-17 13:46:24 -0600

    Added all of the ltspice files

--- Lab7/calcphase.py
+++ Lab7/calcphase.py
@@ -1,7 +1,8 @@
 import numpy as np
 delta_t_list = [60, 28, 17.6, 12.8, 10.4, 8.8, 7.2, 6.2, 5.7, 5, 4.5, 4.12, 3.18, 3.14, 3.24, 3, 2.8, 2.72, 2.56, 2.36]
+delta_t_list = [-220, -116, -58, -25, -8.6, -.68, 2.48, 3.48, 2.76, 2.04]
 delta_t_micro = []
-frequency_list = np.arange(5000, 100001, 5000)
+frequency_list = [1000.0, 1700.0, 2800.0, 4600.0, 7700.0, 12900.0, 21500.0, 35900.0, 59900.0, 100000.0]
 print(frequency_list)
 
 for val in delta_t_list:
@@ -9,3 +10,5 @@ for val in delta_t_list:
 
 for i in range(len(delta_t_list)):
     print(frequency_list[i], round(delta_t_micro[i] * frequency_list[i] * 360, 2), sep='\t')
+for i in range(len(delta_t_list)):
+    print(round(delta_t_micro[i] * frequency_list[i] * 360, 2), sep='\t')
