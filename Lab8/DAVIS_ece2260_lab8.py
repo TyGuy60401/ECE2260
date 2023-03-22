17302feef1323d76a6037858a41165884453c20a
Ty Davis <tydavisxc2019@gmail.com>
2023-03-21 18:59:26 -0600

    Started the lab8 file

---
+++ Lab8/DAVIS_ece2260_lab8.py
@@ -0,0 +1,34 @@
+import scipy as sci
+import matplotlib.pyplot as plt
+
+def main():
+    # For real and distinct
+    return
+
+def inverse_laplace(N_s, D_s, t):
+    """
+    This function will take in the polynomial coefficients
+    N_s and D_s for the Laplace transform as well as a 1D
+    array of time values and compute the inverse Laplace
+    transform for each time value.
+    
+    Parameters
+    ----------
+    N_s:    array_like
+        Array or list of polynomial coefficients for N_s
+    D_s:    array_like
+        Array or list of polynomial coefficients for D_s
+    t:  array_like
+        Array of time values to compute the inverse Laplace
+        transform over
+    
+    Returns
+    -------
+    f:  array_like
+        The values of the inverse laplace transform for each
+        value in the input array t
+    """
+    return
+
+if __name__ == '__main__':
+    main()
