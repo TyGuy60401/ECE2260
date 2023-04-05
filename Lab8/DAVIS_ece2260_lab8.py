af846f57e27136671c028bdbf8c878a34250214c
Ty Davis <tydavisxc2019@gmail.com>
2023-04-04 21:28:35 -0600

    Got it all working

--- Lab8/DAVIS_ece2260_lab8.py
+++ Lab8/DAVIS_ece2260_lab8.py
@@ -1,5 +1,6 @@
 import scipy as sci
 import numpy as np
+import math
 import matplotlib.pyplot as plt
 
 def main():
@@ -8,31 +9,59 @@ def main():
     #             N_s: 96s^2 + 1632s + 5760
     #             D_s: s(s+8)(s+6)
     #             D_s: s&3 + 14s^2 + 48s
+    t = np.linspace(0, 3, 50)
 
-    N_s_1 = np.array([96, 1632, 5760])
+    N_s_1 = np.array([0, 96, 1632, 5760])
     D_s_1 = np.array([1, 14, 48, 0])
-    t_1 = np.array([])
+    
+    td_1 = 120 - 72*np.e**(-8*t) + 48*np.e**(-6*t)
+    out_1 = inverse_laplace(N_s_1, D_s_1, t)
 
     # For complex and distinct
     N_s_2 = np.array([100, 300])
     D_s_2 = np.array([1, 12, 61, 150])
 
+    td_2 = (-12*np.e**(-6*t) + np.e**(-3*t)* (12*np.cos(4*t) + 16*np.sin(4*t)))
+    out_2 = (inverse_laplace(N_s_2, D_s_2, t))
+
     # For real and repeating
     N_s_3 = np.array([100, 2500])
-    D_s_3 = np.array([1, 15, 75, 125])
+    D_s_3 = np.array([1, 15, 75, 125, 0])
+
+    td_3 = 20 - 200*t**2*np.e**(-5*t) - 100*t*np.e**(-5*t) - 20*np.e**(-5*t)
+    out_3 = inverse_laplace(N_s_3, D_s_3, t)
 
     # For complex and repeating
     N_s_4 = np.array([768])
     D_s_4 = np.array([1, 12, 86, 300, 625])
 
-    print("Case 1:")
-    inverse_laplace(N_s_1, D_s_1, 1)
-    print("Case 2:")
-    inverse_laplace(N_s_2, D_s_2, 1)
-    print("Case 3:")
-    inverse_laplace(N_s_3, D_s_3, 1)
-    print("Case 4:")
-    inverse_laplace(N_s_4, D_s_4, 1)
+    td_4 = -24*t*np.e**(-3*t) * np.cos(4*t) + 6*np.e**(-3*t)*np.cos(4*t - np.radians(90))
+    out_4 = inverse_laplace(N_s_4, D_s_4, t)
+
+    # Pyplot stuff
+    fig, axis = plt.subplots(4, figsize=(5,6))
+
+    # ax1 = plt.axes()
+    # fig.add_axes(ax2)
+    axis[0].plot(t, td_1)
+    axis[0].plot(t, out_1, '.', color="xkcd:booger")
+    axis[0].set_title("Real and Distinct")
+
+    # ax2 = plt.axes()
+    # fig.add_axes(ax2)
+    axis[1].plot(t, td_2)
+    axis[1].plot(t, out_2, '.', color="xkcd:baby poop green")
+    axis[1].set_title("Complex and Distinct")
+
+    axis[2].plot(t, td_3)
+    axis[2].plot(t, out_3, '.', color="#ab625c")
+    axis[2].set_title("Real and Repeated")
+
+    axis[3].plot(t, td_4)
+    axis[3].plot(t, out_4, '.', color="#3782b4")
+    axis[3].set_title("Complex and Repeated")
+    plt.tight_layout()
+    plt.show()
 
     
     return
@@ -61,16 +90,32 @@ def inverse_laplace(N_s, D_s, t):
         value in the input array t
     """
     r, poles, res = sci.signal.residue(N_s, D_s)
+    pole = poles[0]
+    f = np.zeros(np.size(t))
     # Test if it's repeated
-    hasRepRoots = False
-    numRepRoots = 0
+    numRepRoots = 1
+    prepole = None
     for i in range(len(poles)):
-        if i == 0:
-            continue
-        if poles[i] == poles[i-1]:
-            # Needs support for multiple repeated roots
+        pole = poles[i]
+        if pole == prepole:
             numRepRoots += 1
-            hasRepRoots = True
+        else:
+            numRepRoots = 1
+        prepole = poles[i]
+        f = f + (r[i] * t **(numRepRoots - 1) * np.exp(poles[i] * t)) / math.factorial(numRepRoots - 1)
+    return f
+
+
+    # for i in range(len(poles)):
+    #     if i == 0:
+    #         continue
+    #     if poles[i] == poles[i-1]:
+    #         # Needs support for multiple repeated roots
+    #         numRepRoots += 1
+    #         hasRepRoots = True
+    #     else:
+    #         numRepRoots = 0
+
 
     hasComplex = np.any(np.iscomplex(r))
     # We probably Dont need this section
